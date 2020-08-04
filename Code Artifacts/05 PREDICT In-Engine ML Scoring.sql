73bbd8f288b3287709f5e00faf4a8dec6d0e8dff
Priyanka Langade <56413766+prlangad@users.noreply.github.com>
2020-08-03 18:45:04 -0700

    Update 05 PREDICT In-Engine ML Scoring.sql

--- Code Artifacts/05 PREDICT In-Engine ML Scoring.sql
+++ Code Artifacts/05 PREDICT In-Engine ML Scoring.sql
@@ -27,32 +27,15 @@ SELECT
 	RANK() OVER (ORDER BY AvgTripAmount DESC) AS RankTripAmount
 FROM (
 	SELECT 
-		   [passengerCount]		as PassengerCount,
-		   COUNT(*)				as TripCount,
-		   AVG([totalAmount])	as AvgTripAmount
-	FROM PREDICT (model = (SELECT Model FROM Models WHERE Id = 60), Data = dbo.TaxiTrips5M) WITH (totalAmount float)
+		   [passengerCount] as PassengerCount,
+		   COUNT(*) as TripCount,
+		   AVG([totalAmount]) as AvgTripAmount
+	FROM PREDICT (model = (SELECT Model FROM Models WHERE Id = 60), Data = dbo.Trips) WITH (totalAmount float)
 	GROUP BY
 		[passengerCount]) SubQ
 ORDER BY
 	PassengerCount
 
-  
--- DROP VIEW
-DROP VIEW vw_PredictedTripAmount
-
--- CREATE VIEW
-CREATE VIEW vw_PredictedTripAmount
-AS
-SELECT [vendorID],
-       [passengerCount],
-       [tripDistance],
-       [month_num],
-       [day_of_month],
-       [day_of_week],
-       [day_of_hour],
-       [totalAmount]
-FROM PREDICT (model = (SELECT Model FROM Models WHERE Id = 59), Data = dbo.TaxiTrips5M) WITH (totalAmount float)
-  
-  
+ 
 -- SELECT TOP 10
 SELECT TOP 10 * FROM vw_PredictedTripAmount
